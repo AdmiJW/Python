b408aa296607592699be647859605bf5af2f3dbd
AdmiJW <jamweg22@gmail.com>
2020-11-13 11:53:56 +0800

    Added Day2 Basic Probability Notes

---
+++ Topics/10 Days Of Statistics/Day2_Basic Probability Notes.py
@@ -0,0 +1,39 @@
+
+# EVENT, SAMPLE SPACE, PROBABILITY
+#
+# An experiment is any procedure that can be infinitely repeated
+# With this, there is a set that consists of all the possible outcomes, known
+# as sample space S
+# An event is a set of outcomes of an experiment which we desire, and a probability
+# value is assigned to it
+#
+# Basically, the probability of occurrence of an event is
+#         P(A) = no of favorable outcomes     = | A |
+#                ------------------------       -----
+#                 total no of outcomes          | S |
+#
+# Remember that, Every probability can only be between 0 and 1, and
+# The probability of the sample space S is always 1
+#
+# A event A can be complemented, A', which the resulting probability is 1 - P(A)
+
+
+
+# When 2 events are combined, A U B, then the probability of it is
+#       P(A U B) = P(A) + P(B) - P(A n B)
+
+
+
+# Two MUTUALLY EXCLUSIVE EVENTS (Also called DISJOINT) means they have
+# No events in common at all.
+#              A n B = {}, P(A n B) = 0
+# Therefore, for Mutually exclusive events, P(A U B) = P(A) + P(B)
+
+# COLLECTIVELY EXHAUSTIVE EVENTS are 2 events, such that their Union
+# Covers all events in sample space.
+#       A U B = S,      P(A) + P(B) = 1
+
+# 2 Events are said to be INDEPENDENT of each other if outcome of the
+# first event doesn't impact the second event. Thus, if two events
+# are independent,
+#       P(A n B) = P(A) * P(B) regardness of any values for A and B
