be550c6cd4a46f0a53ed765666576182c7e0f45f
AdmiJW <jamweg22@gmail.com>
2020-12-26 00:14:33 +0800

    Added Polar Coordinates

--- Topics/Hackerrank/Easy/Polar Coordinates.py
+++ Topics/Hackerrank/Easy/Polar Coordinates.py
@@ -1,3 +1,9 @@
 from cmath import *
 
+# complex() either takes in 2 arguments, each one is float value representing x and y in z=x+yi, or
+# a string representing the complex number
+
+# polar() takes in a complex number object, and returns a tuple containing two values
+#   First is the r value (distance from O)
+#   Second is the phi value (phase angle - radian from quadrant I)
 print( *polar( complex(input() ) ), sep='\n')
