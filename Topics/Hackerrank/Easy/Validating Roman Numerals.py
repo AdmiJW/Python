aaeeef30096b0a496c37a53beace7e6a0635ebfd
AdmiJW <jamweg22@gmail.com>
2021-02-21 12:15:48 +0800

    Added Validating Roman Numerals

---
+++ Topics/Hackerrank/Easy/Validating Roman Numerals.py
@@ -0,0 +1,14 @@
+
+import re
+
+
+# Breakdown of my regular expression:
+#   ^                           -   Starts with
+#   M{0,3}                      -   Can be 0 to 3 'M's
+#   (CM|CD|D?C{0,3})            -   Either CM, CD or optional 'D' followed by at most 3 'C'
+#   (XC|XL|L?X{0,3})            -   Either XC, XL or optional 'L' followed by at most 3 'X'
+#   (IX|IV|V?I{0,3})            -   Either IX, IV or optional 'V' followed by at most 3 'I'
+
+def soln1():
+    pattern = re.compile("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")
+    print( bool(re.fullmatch(pattern, input() ) ) )
