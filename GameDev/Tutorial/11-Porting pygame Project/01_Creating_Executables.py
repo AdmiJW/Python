d9f76cfaa31bb64726ce1da088ce7b9daea9e383
AdmiJW <jamweg22@gmail.com>
2022-03-03 14:00:20 +0800

    Additional notes on distribution

--- GameDev/Tutorial/11-Porting pygame Project/01_Creating_Executables.py
+++ GameDev/Tutorial/11-Porting pygame Project/01_Creating_Executables.py
@@ -2,8 +2,10 @@
 # Note: This doesn't really involve code. This just note how you can compile your game written in pygame so far into
 # a executable file so you can distribute or share to your friends.
 #
+#
 # There are many python executable compiler out there like pyinstaller, cx_freeze, or py2exe. In this note,
 # we'll be using pyinstaller to be consistent to dafluffypotato's tutorial.
+#
 #
 # > First, ensure pyinstaller is installed in your pip globally.
 #       - Open up a command prompt (not virtual environment). Run `pip install pyinstaller`
@@ -21,6 +23,14 @@
 # > However, it may throw some error when you try to run it. The problem is you need to put all your assets (images,
 #   musics etc) into the same structural hierarchy as if you run it in main.py. Copy your assets into the dist folder
 #   to resolve the issue.
+#
+#
+# > Also, if you are developing your program in Pycharm, you may encounter a problem where the pyinstaller couldn't
+#   find the modules that you installed in your venv, but not global python environment.
+#   In this case, you have to provide additional options when running the command, as mentioned in this Stackoverflow
+#   answer: https://stackoverflow.com/questions/48757977/how-to-include-dependencies-from-venv-directory-when-running-pyinstaller-for-pro
+#
+#       pyinstaller --onefile --paths path\to\venv\Lib\site-packages main.py
 
 
 # Tips:
